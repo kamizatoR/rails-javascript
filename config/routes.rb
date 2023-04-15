9bc33faca9a4b2cf0e2d8cddd63ed9cc31ab4543
kr <112645912+kamizatoR@users.noreply.github.com>
2023-04-15 18:08:57 +0000

    create controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'notifications/create'
+  get 'notifications/index'
+  get 'likes/create'
+  get 'likes/destroy'
+  get 'post/new'
+  get 'post/create'
+  get 'post/destroy'
+  get 'post/show'
+  get 'follows/create'
+  get 'follows/destroy'
+  get 'users/show'
+  get 'users/destroy'
   devise_for :users
   get 'tests/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
